02243dd8fe95a6e1742fc2b6ad2b47b0ee43d190
Aleksandra Prokopets <stormofe@gmail.com>
2021-09-24 17:47:29 +0300

    Start remembering React. Use hooks (useState, useRef), create components and UI components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import Counter from "./components/Counter";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import "./styles/App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [posts, setPosts] = useState([
+		{ id: 1, title: "JS", body: "Description" },
+		{ id: 2, title: "JS2", body: "Description" },
+		{ id: 3, title: "JS3", body: "Description" },
+	]);
+
+	const [title, setTitle] = useState("");
+	const [body, setBody] = useState("");
+	//const bodyInputRef = useRef();
+
+	const addNewPost = (e) => {
+		e.preventDefault();
+		//console.log(title);
+		//console.log(bodyInputRef.current.value);
+
+		const newPost = {
+			id: Date.now(),
+			title,
+			body,
+		};
+		//console.log(newPost);
+		setPosts([...posts, newPost]);
+		setTitle("");
+		setBody("");
+	};
+
+	return (
+		<div className='App'>
+			<form>
+				{/*Управляемый компонент*/}
+				<MyInput value={title} onChange={(e) => setTitle(e.target.value)} type='text' placeholder='Name of project' />
+				<MyInput value={body} onChange={(e) => setBody(e.target.value)} type='text' placeholder='Description' />
+				{/*Неуправляемый компонент*/}
+				{/*<MyInput ref={bodyInputRef} type='text' placeholder='Description' />*/}
+				<MyButton onClick={addNewPost}>Add post</MyButton>
+			</form>
+			<PostList posts={posts} title={"List of posts"} />
+			<Counter />
+			<Counter />
+			<Counter />
+			<Counter />
+			<Counter />
+		</div>
+	);
 }
 
 export default App;
